b357bbabab9e6c4d6c9aa04d92dc43ac99b5fa5c
John Paul Kpantey <john.kpantey@turntabl.io>
2023-04-18 11:22:32 +0000

    refactor: Rename tests methods and change assertTrue to assertEquals
    Renamed tests methods according to the PR corrections and changed assertTrue to assertEquals

--- src/test/java/com/ttrides/turntablRides/controllers/UserControllerTest.java
+++ src/test/java/com/ttrides/turntablRides/controllers/UserControllerTest.java
@@ -28,7 +28,7 @@ class UserControllerTest {
     @BeforeEach
     void setUp() {
         // Must update to a fresh token to test
-        correctToken = "eyJhbGciOiJSUzI1NiIsImtpZCI6ImFjZGEzNjBmYjM2Y2QxNWZmODNhZjgzZTE3M2Y0N2ZmYzM2ZDExMWMiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiI0NTk2MTEyODcxMDYtdmEyZGszaWcycDJ2YW03dGZydXY4OHVmdDRvdWVibzkuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiI0NTk2MTEyODcxMDYtdmEyZGszaWcycDJ2YW03dGZydXY4OHVmdDRvdWVibzkuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMTAyMjQ4NzEzMDQxNTAxMDA0NzIiLCJoZCI6InR1cm50YWJsLmlvIiwiZW1haWwiOiJqb2huLmtwYW50ZXlAdHVybnRhYmwuaW8iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiYXRfaGFzaCI6IjBEaFNrcXFkMC1zRWpkY3pGNUlHblEiLCJuYW1lIjoiSm9obiBQYXVsIEtwYW50ZXkiLCJwaWN0dXJlIjoiaHR0cHM6Ly9saDMuZ29vZ2xldXNlcmNvbnRlbnQuY29tL2EvQUdObXl4WWtQUUV2SEFsMncxTlRXUEt6ZXliNVQ2WHBlZVV3NTM4blg0dnY9czk2LWMiLCJnaXZlbl9uYW1lIjoiSm9obiBQYXVsIiwiZmFtaWx5X25hbWUiOiJLcGFudGV5IiwibG9jYWxlIjoiZW4iLCJpYXQiOjE2ODEzMjM0MzgsImV4cCI6MTY4MTMyNzAzOH0.SLRJ91-jnxsRg9PBbq1O40ZZ9DqqSbuhnRqM0oyxBG4-WYLLs_tVm7duNctaIoESKi2sXe7Pcv_OXCBuxUVhlHnWSTndTIoySYBC4OZ_EmoFk-XkOfHbHuh6YYIgAgrb3Fko_Mvnf4dCB5ahIuwh0o8WEqZNZSLpY5oHJ-m4E7Sgl_8E_lmvTMz3sHi1Ll8_NRcmaeZ_dS_LWlPDO0LD2nFvJiuzSw0w_803Fh1SI4mwwZzMFUwpp6VJpSfaLX81E4GjbhZ4QT3wHAFd2DH5VS2Lw6u6rjxpXQAA-cQIJPnoedJeN-jdXO2Ye72r0kJ2NTSwVCEhUWh5umzAAF_F-A";
+        correctToken = "eyJhbGciOiJSUzI1NiIsImtpZCI6Ijk2OTcxODA4Nzk2ODI5YTk3MmU3OWE5ZDFhOWZmZjExY2Q2MWIxZTMiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiI3NzI2NTQzNDAxODgtOWNvbzkwMm9kOG1pMDdhZmE3Mmo0amtwZjliMTlmbTguYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiI3NzI2NTQzNDAxODgtOWNvbzkwMm9kOG1pMDdhZmE3Mmo0amtwZjliMTlmbTguYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMTAyMjQ4NzEzMDQxNTAxMDA0NzIiLCJoZCI6InR1cm50YWJsLmlvIiwiZW1haWwiOiJqb2huLmtwYW50ZXlAdHVybnRhYmwuaW8iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiYXRfaGFzaCI6ImFNLS1CdUFXaGZncmZWYlFVOGYwa2ciLCJuYW1lIjoiSm9obiBQYXVsIEtwYW50ZXkiLCJnaXZlbl9uYW1lIjoiSm9obiBQYXVsIiwiZmFtaWx5X25hbWUiOiJLcGFudGV5IiwibG9jYWxlIjoiZW4iLCJpYXQiOjE2ODE4MTUxNTksImV4cCI6MTY4MTgxODc1OX0.pqxzlxNQT3nn5pxb6jGEcVXaJ8-Lvsg68Sg7ChrsFfjtVS3YBX1DCn8XLmED_hCFXjmbXw9Nru3rN2DjBXjkfDR_pm63aLOfcmGbNIjYoiYuZNQx4YD0rnZ2k5qR5C8gbRIBRcaA2JQZq4Oq_7bg7lErzXL54rOfgSyk3TVCr7RpSfBhi7m3irugpPwcQ44Iher3ac5hoJfQIVaMv5eKbwjst0R8lW-pG1N0SGXDU8FNRYF2WaI00OXsvZDoKdXhlXhEmRSJpbfGoEi-Hkk8DftKtY7144AzixAEygqf2ZG2W-2-g7rgQgtLSBqy8tEfqeA07eyfThIMQJJRMN1TdA";
         // Dead token or incorrect token
         unauthorisedToken = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjFhYWU4ZDdjOTIwNThiNWVlYTQ1Njg5NWJmODkwODQ1NzFlMzA2ZjMiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiI3NzI2NTQzNDAxODgtOWNvbzkwMm9kOG1pMDdhZmE3Mmo0amtwZjliMTlmbTguYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiI3NzI2NTQzNDAxODgtOWNvbzkwMm9kOG1pMDdhZmE3Mmo0amtwZjliMTlmbTguYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMTAyMjQ4NzEzMDQxNTAxMDA0NzIiLCJoZCI6InR1cm50YWJsLmlvIiwiZW1haWwiOiJqb2huLmtwYW50ZXlAdHVybnRhYmwuaW8iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiYXRfaGFzaCI6InhBanZVSS1wblY2dnNLOHM5eFhYU1EiLCJuYW1lIjoiSm9obiBQYXVsIEtwYW50ZXkiLCJwaWN0dXJlIjoiaHR0cHM6Ly9saDMuZ29vZ2xldXNlcmNvbnRlbnQuY29tL2EvQUdObXl4WWtQUUV2SEFsMncxTlRXUEt6ZXliNVQ2WHBlZVV3NTM4blg0dnY9czk2LWMiLCJnaXZlbl9uYW1lIjoiSm9obiBQYXVsIiwiZmFtaWx5X25hbWUiOiJLcGFudGV5IiwibG9jYWxlIjoiZW4iLCJpYXQiOjE2ODA1Mzg0MjksImV4cCI6MTY4MDU0MjAyOX0.XLY2lsIBwjSs6v4nbe5T5CmuBbTpguRb3Hwn1Wm-izYjdROK_a0Ff7EqV4Ztwhg_-BNziF0lIZLbvyeCgOfxT10_OwzDCjrr3PB_1gWqqfCvFMwdvz-OgVuBhg7qyCv5GAJfJCuqu0MCmmWiai_EI68fBG8LTiZf_upVaZAvSJrjNpGQTvGzXK7VpKL4DpTJwVCSQ4aiU94wIDzrPeej0JsOQT0WdtAOpzBz9xY9cwD7QAPCHbcsrIEkdyqKdybRgoXDRnJnJnKjtkC3o9mpFy6cPJeT4n5Mr-LCHSHoRzPezaFOWMsKeHV_ac9w1IVbvGELK1PFGR1oQ9ZHCMsdcQ";
 
@@ -39,7 +39,7 @@ class UserControllerTest {
     }
 
     @Test
-    void loginUser_whenProvidedUnauthorisedOrExpiredOrInvalidToken_ShouldReturnInvalid_tokenWith401Exception() throws Exception {
+    void testLoginUserWithInvalidTokenReturns401ExceptionAndInvalidTokenErrorMessage() throws Exception {
         MvcResult errorResult = mockMvc.perform(get("/api/v1/login")
                                         .header("Authorization", "Bearer " + unauthorisedToken))
                                         .andExpect(status().isUnauthorized())
@@ -47,11 +47,12 @@ class UserControllerTest {
 
         String content = (String) errorResult.getResponse().getHeaderValue("WWW-Authenticate");
         assert content != null;
-        assertTrue(content.contains("invalid_token"));
+        assertEquals("invalid_token", content);
+
     }
 
     @Test
-    void loginUser_WhenProvidedCorrectToken_ShouldBeOkWith200ResponseAndExactResponseObject() throws Exception{
+    void testLoginUserWithCorrectTokenReturnsExpectedResponse() throws Exception{
         MvcResult result = mockMvc.perform(get("/api/v1/login").header("Authorization", "Bearer " + correctToken))
                                     .andExpect(status().isOk())
                                     .andReturn();
