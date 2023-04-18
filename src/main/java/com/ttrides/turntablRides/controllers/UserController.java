558e3e48dfd042a79fcf7f5b649ad741ad45161e
John Paul Kpantey <john.kpantey@turntabl.io>
2023-04-18 14:40:50 +0000

    chore: Remove unused module

--- src/main/java/com/ttrides/turntablRides/controllers/UserController.java
+++ src/main/java/com/ttrides/turntablRides/controllers/UserController.java
@@ -5,7 +5,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
